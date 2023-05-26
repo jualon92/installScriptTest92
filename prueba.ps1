78970535829355e6c3e522faa15d7de20d021ac8
Juan Ignacio Londra <46230600+jualon92@users.noreply.github.com>
2023-05-26 10:38:42 -0300

    Update prueba.ps1

--- prueba.ps1
+++ prueba.ps1
@@ -1,2 +1,14 @@
-write-host "Hello World"
-choco 
+Set-ExecutionPolicy AllSigned
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install -y adobereader
+choco install -y firefox 
+choco install -y chrome
+choco install -y  javaruntime
+choco install -y  jre8
+choco install -y  7zip
+choco install -y  winrar
+choco install -y  cutepdf
+choco install -y  jdk8
+choco install -y anydesk.install 
+choco install -y vlc 
+
